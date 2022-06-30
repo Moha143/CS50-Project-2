38a6c510f0657a69f287a60f1e6b5fc875d5cce7
Moha143 <moha143omar@gmail.com>
2022-06-30 21:09:25 +0300

    completed project

--- auctions/models.py
+++ auctions/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+#USER MODEL
 class User(AbstractUser):
     pass
-
+# 1.auctionlist model
 class auctionlist(models.Model):
     user = models.CharField(max_length=64)
     title = models.CharField(max_length=64)
@@ -12,23 +12,23 @@ class auctionlist(models.Model):
     image_url = models.CharField(max_length=228, default = None, blank = True, null = True)
     category = models.CharField(max_length=64)
     active_bool = models.BooleanField(default = True)
-
+# 2.bids model
 class bids(models.Model):
     user = models.CharField(max_length=30)
     listingid = models.IntegerField()
     bid = models.IntegerField()
     
-
+# 3.comments model
 class comments(models.Model):
     user = models.CharField(max_length=64)
     comment = models.TextField()
     listingid = models.IntegerField()
     
-
+# 4.watchlist model
 class watchlist(models.Model):
     watch_list = models.ForeignKey(auctionlist, on_delete=models.CASCADE)
     user = models.CharField(max_length=64)
-
+# 5.winner model
 class winner(models.Model):
     bid_win_list = models.ForeignKey(auctionlist, on_delete = models.CASCADE)
     user = models.CharField(max_length=64, default = None)
